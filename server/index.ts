d3f8889658b6f19d60368884be926c57efb31379
JNaftali <jmarantz@thelabnyc.com>
2020-04-27 03:19:58 -0400

    renders one page

--- server/index.ts
+++ server/index.ts
@@ -1,30 +1,37 @@
-import { createServer } from 'http'
-import { parse } from 'url'
-import next from 'next'
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
+import fetch from 'node-fetch';
 
-const port = parseInt(process.env.PORT || '3000', 10)
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT || '3000', 10);
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    const { pathname, query } = parsedUrl
+  createServer(async (req, res) => {
+    const parsedUrl = parse(req.url!, true);
+    const { pathname, query } = parsedUrl;
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
+    const cmsResponse = await fetch(
+      `https://api-dev.devatech.us/api/v2/pages/find/?site=2&html_path=${pathname}`,
+    );
+
+    if (cmsResponse.status >= 400) {
+      handle(req, res, parsedUrl);
     } else {
-      handle(req, res, parsedUrl)
+      const cmsData = await cmsResponse.json();
+      app.render(req, res, `/wagtail/${cmsData.meta.type}`, {
+        ...query,
+        wagtailPageId: cmsData.id,
+      });
     }
-  }).listen(port)
+  }).listen(port);
 
   // tslint:disable-next-line:no-console
   console.log(
     `> Server listening at http://localhost:${port} as ${
       dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
-})
+    }`,
+  );
+});
