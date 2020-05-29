0bebb1e5211af6c1f8b2961eae9b8a6ad5076bf5
JNaftali <jmarantz@thelabnyc.com>
2020-05-29 13:50:28 -0400

    using "as"

--- src/components/NavOrSomething.tsx
+++ src/components/NavOrSomething.tsx
@@ -4,22 +4,28 @@ export function NavOrSomething() {
   return (
     <ul>
       <li>
-        <Link href="/us">
+        <Link href="/wagtail/cms.HomePage" as="/us">
           <a>Home</a>
         </Link>
       </li>
       <li>
-        <Link href="/us/products">
+        <Link href="/wagtail/cms.ProductGridListPage" as="/us/products">
           <a>All</a>
         </Link>
       </li>
       <li>
-        <Link href="/us/products/cleansers">
+        <Link
+          href="/wagtail/cms.ProductGridListPage"
+          as="/us/products/cleansers"
+        >
           <a>Cleansers</a>
         </Link>
       </li>
       <li>
-        <Link href="/us/products/conditioners">
+        <Link
+          href="/wagtail/cms.ProductGridListPage"
+          as="/us/products/conditioners"
+        >
           <a>Conditioners</a>
         </Link>
       </li>
